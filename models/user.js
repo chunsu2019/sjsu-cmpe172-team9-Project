6e2f67d4ed4486fcc5a68bbdffe4099396e6c665
JianBin Wu <70332991+jiwu54@users.noreply.github.com>
2021-11-02 15:24:59 -0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -3,10 +3,7 @@ const Schema = mongoose.Schema
 const passportLocalMongoose = require('passport-local-mongoose')
 const findOrCreate = require('mongoose-findorcreate')
 
-const UserSchema = new Schema({
-    username: {type: String, required: true},
-    password: {type: String, required: true}
-})
+const UserSchema = new Schema({})
 
 UserSchema.plugin(passportLocalMongoose)
 UserSchema.plugin(findOrCreate)
